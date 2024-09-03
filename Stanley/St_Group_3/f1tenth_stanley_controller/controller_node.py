96d0603dca37d07adcfcab4a02215498897b59bf
ninzzd <ninaad.desai1212@gmail.com>
2024-09-03 21:25:59 +0530

    Updating the package developed by Group 3. Working Stanley V3

--- Stanley/St_Group_3/f1tenth_stanley_controller/controller_node.py
+++ Stanley/St_Group_3/f1tenth_stanley_controller/controller_node.py
@@ -9,6 +9,7 @@ from tf2_ros import Buffer
 from tf2_ros import TransformException
 from tf2_ros.transform_listener import TransformListener
 from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import PointStamped
 from geometry_msgs.msg import Quaternion
@@ -26,9 +27,9 @@ class StanleyController(Node):
     def __init__(self):
         super().__init__('stanley_controller')
         # TODO: create ROS subscribers and publishers
-        self.k = 3
+        self.k = 4
         self.ks = 2.5
-        self.vel_max = 0.5
+        self.vel_max = 1
         self.vel = 0.5
         self.angle_limit = np.pi/4
         self.flw = 'ego_racecar/front_left_wheel'
@@ -41,6 +42,7 @@ class StanleyController(Node):
 
         #Assuming that the waypoints are delivered to us in the right chronological sequence (probably assigned by some ID)
         self.waypoints = []
+        self.vel_vector:list
         self.getWaypoints()
         print(f'No. of waypoints: {len(self.waypoints)}')
         self.goal = 0
@@ -48,6 +50,7 @@ class StanleyController(Node):
         self.publisher = self.create_publisher(AckermannDriveStamped,'/drive',qos_profile=qos)
         self.vel_pub = self.create_publisher(Twist,'/cmd_vel',qos_profile=qos)
         self.goal_pub = self.create_publisher(PointStamped,'/local_goal_point',qos_profile=qos)
+        self.odom_subscriber = self.create_subscription(Odometry,'/ego_racecar/odom',qos_profile=qos,callback=self.getTwist)
         # Higher the rate of execution, lower the error caused by delayed messages
         self.timer = self.create_timer(timer_period_sec=0.05,callback=self.compute)
 
@@ -55,6 +58,12 @@ class StanleyController(Node):
         with open(file=self.wayfile,mode='r') as file:
             reader = csv.reader(file)
             self.waypoints = list(reader)
+            
+    def getTwist(self,msg:Odometry):
+        x = float(msg.twist.twist.linear.x)
+        y = float(msg.twist.twist.linear.y)
+        self.vel_vector = [x,y,0.0]
+        return
     
     def compute(self):
         try:
@@ -76,7 +85,7 @@ class StanleyController(Node):
                     break
                 except TransformException as e:
                     self.get_logger().warn(f'Could not transform: {e}')
-            
+
             x0 = float(t_flw.transform.translation.x + t_frw.transform.translation.x)/2
             y0 = float(t_flw.transform.translation.y + t_frw.transform.translation.y)/2 
             if self.goal < len(self.waypoints) - 1:
@@ -87,49 +96,77 @@ class StanleyController(Node):
                 quart.y = t_heading.transform.rotation.y
                 quart.z = t_heading.transform.rotation.z
                 quart.w = t_heading.transform.rotation.w
-                _,_,yaw = euler.quat2euler([quart.w,quart.x,quart.y,quart.z])
-                dist = float("inf")
-                i:int
-                for i in range(len(self.waypoints)):
-                    x = float(self.waypoints[i][0])
-                    y = float(self.waypoints[i][1])
-                    dist_ = np.sqrt(np.power(x-x0,2)+np.power(y-y0,2))
-                    if dist_<dist:
-                        dist = dist_
-                        self.goal = i
-                self.goal+=1
 
+                # -pi < yaw <= pi
+                _,_,yaw = euler.quat2euler([quart.w,quart.x,quart.y,quart.z])
+
+                # Estimation of goal waypoints
+                while self.goal < len(self.waypoints)-1:
+                    x1 = float(self.waypoints[self.goal][0])
+                    y1 = float(self.waypoints[self.goal][1])
+                    x2 = float(self.waypoints[self.goal+1][0])
+                    y2 = float(self.waypoints[self.goal+1][1])
+                    dist1 = np.sqrt(np.square(x1-x0) + np.square(y1-y0))
+                    dist2 = np.sqrt(np.square(x2-x0) + np.square(y2-y0))
+                    if(dist1 < dist2): 
+                        break
+                    else:
+                        self.goal+=1
+
+                # Goal waypoint visualisation
                 p = PointStamped()
                 p.header.frame_id = 'map'
                 p.header.stamp = self.get_clock().now().to_msg()
-                x1 = float(self.waypoints[self.goal][0])
-                y1 = float(self.waypoints[self.goal][1])
                 p.point.x = x1
                 p.point.y = y1
                 p.point.z = z0
                 self.goal_pub.publish(p)
+                print(f'\nGoal: {self.waypoints[self.goal]}')
 
-                x2 = float(self.waypoints[self.goal+1][0])
-                y2 = float(self.waypoints[self.goal+1][1])
-                print(f'Goal: {self.waypoints[self.goal]}')
-                m = (y2 - y1)/(x2 - x1)
-                c = y1 - m*x1
+                # Coordinate Geometry 
+                a = y2 - y1
+                b = x1 - x2
+                c = y1*(x2-x1) - x1*(y2-y1)
 
-                e_t = abs(m*x0 - y0 + c)/np.sqrt(1 + m*m)
-                psi_t = np.arctan((m-np.tan(yaw))/(1+m*np.tan(yaw)))
+                # Crosstrack error estimation
+                e_t = abs(a*x0 + b*y0 + c)/np.sqrt(a*a + b*b)
+
+                #------------------------------------------------------------------------------------
+                # Angle of line estimation
+                # Angle space: -pi < angle < +pi 
+                # When 0 < angle < pi/2:    sin > 0, cos > 0 => B > 0, A > 0
+                # When pi/2 < angle < pi:   sin > 0, cos < 0 => B > 0, A < 0  (requires adjustment)
+                # When -pi/2 < angle < 0:   sin < 0, cos > 0 => B < 0, A > 0
+                # When -pi < angle < -pi/2: sin < 0, cos < 0 => B < 0, A < 0  (requires adjustment)
+                alpha = np.arcsin(a/np.sqrt(a*a + b*b))
+                theta:float
+                if(b > 0 and a < 0):
+                    theta = np.pi - alpha
+                elif(b < 0 and a < 0):
+                    theta = -np.pi - alpha
+                else:
+                    theta = alpha
+
+                #------------------------------------------------------------------------------------
+                # Heading error estimation
+                psi_t = theta - yaw
+                
 
                 print(f'Velocity = {self.vel}')
+                print(f'Theta = {theta*180/np.pi}, Yaw = {yaw*180/np.pi}')
                 print(f'Crosstrack Error = {e_t}, Heading Error = {psi_t*180/np.pi} deg')
 
                 # Stanley Controller Algorithm
-                delta_t = psi_t + np.sign(m*x0 - y0 + c)*np.arctan(self.k*(e_t/(self.ks + self.vel)))
+                delta_t = psi_t + np.sign(a*x0 + b*y0 + c)*np.arctan(self.k*(e_t/(self.ks + self.vel)))
+                # Large heading error approximation
                 if(abs(delta_t) >= self.angle_limit): 
-                    delta_t = np.sign(m*x0 - y0 + c)*self.angle_limit
-                print(f'Output steering angle: {delta_t}')
+                    delta_t = np.sign(a*x0 + b*y0 + c)*self.angle_limit
+
+                print(f'Output steering angle: {delta_t}\n')
                 msg:AckermannDriveStamped = AckermannDriveStamped()
                 msg.header.stamp = self.get_clock().now().to_msg()
                 msg.header.frame_id = "map"
-                msg.drive.steering_angle = delta_t
+                msg.drive.steering_angle = float(delta_t)
 
                 # This means that the change in steering angle in the sim happens almost instantly after the message is received (No delay)
                 msg.drive.steering_angle_velocity = float(0.0) 
